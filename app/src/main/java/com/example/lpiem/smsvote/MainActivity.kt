df38515123deb8a71b292bc4dbf5fc568de44aab
Brandon <brandon.gosse@worldline.com>
2019-01-07 15:35:40 +0100

    PermisionUtil

--- app/src/main/java/com/example/lpiem/smsvote/MainActivity.kt
+++ app/src/main/java/com/example/lpiem/smsvote/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.lpiem.smsvote
 
+import android.Manifest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lpiem.smsvote.utils.PermisionUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        PermisionUtil.askForPermission(this, Manifest.permission.RECEIVE_SMS, 15)
     }
 }

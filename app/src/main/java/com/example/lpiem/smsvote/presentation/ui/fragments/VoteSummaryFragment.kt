9f82b42084057c028885bc092f7fc34f8811e022
Jordan Bonnot <jojobonnot@gmail.com>
2019-01-08 10:25:58 +0100

    Vote Summary Screen

--- app/src/main/java/com/example/lpiem/smsvote/presentation/ui/fragments/VoteSummaryFragment.kt
+++ app/src/main/java/com/example/lpiem/smsvote/presentation/ui/fragments/VoteSummaryFragment.kt
@@ -7,13 +7,14 @@ import com.example.lpiem.smsvote.base.BaseFragment
 import com.example.lpiem.smsvote.base.BasePresenter
 import com.example.lpiem.smsvote.base.BaseView
 import com.example.lpiem.smsvote.presentation.presenter.VoteCreationFragmentPresenter
-import com.example.lpiem.smsvote.presentation.presenter.VoteCretionView
+import com.example.lpiem.smsvote.presentation.presenter.VoteSummaryFragmentPresenter
+import com.example.lpiem.smsvote.presentation.presenter.VoteSummaryView
 import com.example.lpiem.smsvote.utils.PermisionUtil
 
-class VoteCreationFragment : BaseFragment<VoteCreationFragmentPresenter>(), VoteCretionView {
+class VoteSummaryFragment: BaseFragment<VoteSummaryFragmentPresenter>(), VoteSummaryView {
 
-    override val layoutId: Int = R.layout.vote_creation_fragment
-    override var presenter: VoteCreationFragmentPresenter = VoteCreationFragmentPresenter()
+    override val layoutId: Int = R.layout.vote_summary_fragment
+    override var presenter: VoteSummaryFragmentPresenter = VoteSummaryFragmentPresenter()
 
     override fun displayLoader() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -29,9 +30,6 @@ class VoteCreationFragment : BaseFragment<VoteCreationFragmentPresenter>(), Vote
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        presenter = VoteCreationFragmentPresenter()
         presenter.attach(this)
-        PermisionUtil.askForPermission(activity!!, Manifest.permission.RECEIVE_SMS, 15)
     }
-
 }
